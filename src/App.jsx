818f9983e351b7c15112ce2002c7e8fd08a4884b
Athul-krish <athulkrishnapanamakkal@gmail.com>
2026-07-11 12:12:06 +0530

    Navbar Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import AddRegister from "./components/AddRegister"
-import DeleteRegister from "./components/DeleteRegister"
-import SearchRegister from "./components/SearchRegister"
-import ViewRegister from "./components/ViewRegister"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AddRegister from "./components/AddRegister";
+import SearchRegister from "./components/SearchRegister";
+import DeleteRegister from "./components/DeleteRegister";
+import ViewRegister from "./components/ViewRegister";
 
 function App() {
-
   return (
-    <>
-      <AddRegister/>
-      <SearchRegister/>
-      <DeleteRegister/>
-      <ViewRegister/>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AddRegister />} />
+        <Route path="/search" element={<SearchRegister />} />
+        <Route path="/delete" element={<DeleteRegister />} />
+        <Route path="/view" element={<ViewRegister />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
